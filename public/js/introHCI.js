219457f637aebbbbdefee517b50c811c5d4f05da
Anjelica Thang <amthang@ucsd.edu>
2016-02-11 16:38:41 +0000

    Finished lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,18 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project" + idNumber, addProjInfo);
+	console.log("Calling URL for: /project/" + idNumber);
+}
+
+
+function addProjInfo(result){
+	console.log(result);
+	var idNumber = result['id'];
+	var projHTML = '<img src="' + result['image'] + '" class = detailsImage">' + '<p>' + result['summary'] + '</p>' + 
+	 '<p><small>' + result['date'] + '</small></p>';
+	$("#project" + idNumber + " .details").html(projHTML);
 }
 
 /*
@@ -34,5 +46,17 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
+	e.preventDefault();
 	console.log("User clicked on color button");
+	$.get("/palette", randomColors);
+}
+
+function randomColors(result){
+	var hex = result['colors'];
+	var colors = hex['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
